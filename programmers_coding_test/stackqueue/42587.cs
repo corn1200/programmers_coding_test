1c5715178e288af01104b609442692cff935c165
김현승 <corn120033@gmail.com>
2024-05-01 17:02:49 +0900

    스택/큐 > 프로세스 문제 풀이 추가(큐 & Linq 사용, 속도 빠름, 평균 1.0ms)

---
+++ programmers_coding_test/stackqueue/42587.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    class Process
+    {
+        public int location;
+        public int priority;
+
+        public Process(int location, int priority)
+        {
+            this.location = location;
+            this.priority = priority;
+        }
+    }
+    
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<Process> queue = new Queue<Process>();
+
+        for (int i = 0; i < priorities.Length; i++)
+        {
+            queue.Enqueue(new Process(i, priorities[i]));
+        }
+
+        while (queue.Count > 0)
+        {
+            Process current = queue.Dequeue();
+            var query = queue.Count(e => e.priority > current.priority);
+            if (query > 0)
+            {
+                queue.Enqueue(current);
+            }
+            else
+            {
+                answer++;
+                if (current.location == location)
+                {
+                    return answer;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
