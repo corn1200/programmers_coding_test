008475cfcf6edce4876a11b6655211a5e7595983
김현승 <corn120033@gmail.com>
2024-05-01 16:04:34 +0900

    스택/큐 > 기능개발 문제 풀이 추가(큐 사용, 속도 빠름, 평균 1.5ms)

---
+++ programmers_coding_test/stackqueue/42586.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<int> answer = new List<int>();
+        Queue<double> queue = new Queue<double>();
+        
+        for (int i = 0; i < progresses.Length; i++)
+        {
+            queue.Enqueue(Math.Ceiling((double) (100 - progresses[i]) / (double) speeds[i]));
+        }
+
+        int currentDay = -1;
+        int listIndex = -1;
+        while (queue.Count > 0)
+        {
+            double queueResult = queue.Dequeue();
+            if (currentDay < queueResult)
+            {
+                currentDay = (int)queueResult;
+                answer.Add(1);
+                listIndex++;
+            }
+            else
+            {
+                answer[listIndex]++;
+            }
+        }
+        
+        return answer.ToArray();
+    }
+}
