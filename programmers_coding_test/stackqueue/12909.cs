360c14e1dc39131ef28decb11cea76095d89c4db
김현승 <corn120033@gmail.com>
2024-05-01 16:15:59 +0900

    스택/큐 > 올바른 괄호 문제 풀이 추가(스택 사용, 속도 빠름, 평균 2.0ms)

---
+++ programmers_coding_test/stackqueue/12909.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public bool solution(string s) {
+        bool answer = true;
+        Stack<char> bracket = new Stack<char>();
+        
+        foreach (var c in s.ToArray())
+        {
+            switch (c)
+            {
+                case '(':
+                    bracket.Push(c);
+                    break;
+                case ')':
+                    if (bracket.Count > 0)
+                    {
+                        bracket.Pop();
+                    }
+                    else
+                    {
+                        answer = false;
+                        return answer;
+                    }
+                    break;
+            }            
+        }
+
+        answer = bracket.Count == 0;
+        return answer;
+    }
+}
