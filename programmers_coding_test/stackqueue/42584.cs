9849d6bdf565954dbfd3b6bd12f33560c5d50459
김현승 <corn120033@gmail.com>
2024-05-01 19:04:08 +0900

    스택/큐 > 주식가격 문제 풀이 추가(배열 사용, 속도 빠름, 평균 2.0ms)

---
+++ programmers_coding_test/stackqueue/42584.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] prices) {
+        List<int> answer = new List<int>();
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            int time = -1;
+            for (int j = i; j < prices.Length; j++)
+            {
+                time++;
+                if (prices[i] > prices[j])
+                {
+                    break;
+                }
+            }
+            answer.Add(time);
+        }
+        
+        return answer.ToArray();
+    }
+}
