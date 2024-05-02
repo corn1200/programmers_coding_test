c240f14d2e8effeb3f00286d769a8df9ab551fcf
김현승 <corn120033@gmail.com>
2024-05-02 12:28:09 +0900

    완전탐색 > 최소직사각형 문제 풀이 추가(속도 빠름, 평균 0.3ms)

---
+++ programmers_coding_test/bruteforce/86491.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Solution {
+    public int solution(int[,] sizes) {
+        int width = 0;
+        int length = 0;
+
+        for (int i = 0; i < sizes.GetLength(0); i++)
+        {
+            int currWidth = sizes[i, 0]; 
+            int currLength = sizes[i, 1];
+            if (currWidth > currLength)
+            {
+                int temp = currWidth;
+                currWidth = currLength;
+                currLength = temp;
+            }
+            width = width < currWidth ? currWidth : width;
+            length = length < currLength ? currLength : length;
+        }
+        
+        return width * length;
+    }
+}
