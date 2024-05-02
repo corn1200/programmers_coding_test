3cbc39cc542b7f4e24420c13f6e7d04be497e714
김현승 <corn120033@gmail.com>
2024-05-02 12:15:37 +0900

    정렬 > H-Index 문제 풀이 추가(List Sort & Linq 사용, 속도 빠름, 평균 3.0ms)

---
+++ programmers_coding_test/sort/42747.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        List<int> copy = new List<int>(citations);
+        copy.Sort();
+
+        for (int i = copy.Count; i >= 0 ; i--)
+        {
+            if (copy.Count(x => i <= x) >= i && copy.Count(x => i >= x) <= i)
+            {
+                return i;
+            }
+        }
+        
+        return answer;
+    }
+}
