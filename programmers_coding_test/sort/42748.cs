146d742ce79e05bf474321ddce7e47af9b91637e
김현승 <corn120033@gmail.com>
2024-05-02 11:23:26 +0900

    정렬 > K번째수 문제 풀이 추가(List Sort 사용, 속도 빠름, 평균 3.0ms)

---
+++ programmers_coding_test/sort/42748.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] array, int[,] commands) {
+        List<int> answer = new List<int>();
+
+        for (int i = 0; i < commands.GetLength(0); i++)
+        {
+            int iCom = commands[i, 0];
+            int jCom = commands[i, 1];
+            int kCom = commands[i, 2];
+
+            List<int> copy = array.Skip(iCom - 1).Take(jCom - iCom + 1).ToList();
+            copy.Sort();
+            answer.Add(copy[kCom - 1]);
+        }
+        
+        return answer.ToArray();
+    }
+}
