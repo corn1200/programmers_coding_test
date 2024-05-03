48f7e680d6dc960848d487595f5571c0a7fbcc82
김현승 <corn120033@gmail.com>
2024-05-03 11:07:12 +0900

    동적계획법(Dynamic Programming) > 피보나치 수(DP 사용, 속도 빠름, 평균 1.0ms)

---
+++ programmers_coding_test/dynamicprogramming/12945.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    private static int[] memo = new int[]{};
+    
+    public int solution(int n) {
+        int answer = 0;
+
+        memo = new int[n + 1];
+        answer = fib(n);
+        
+        return answer;
+    }
+
+    static int fib(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        if (memo[n] != 0)
+        {
+            return memo[n];
+        }
+
+        memo[n] = (fib(n - 1) + fib(n - 2)) % 1234567;
+        return memo[n];
+    }
+}
