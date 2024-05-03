48b02348ba465bd625cbde6ff420ab1f7db42309
김현승 <corn120033@gmail.com>
2024-05-03 18:16:45 +0900

    깊이/너비 우선 탐색(DFS/BFS) > 단어 변환(BFS 사용, 속도 빠름, 평균 1.0ms)

---
+++ programmers_coding_test/dfsbfs/43163.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(string begin, string target, string[] words)
+    {
+        if (Array.IndexOf(words, target) < 0)
+        {
+            return 0;
+        }
+
+        bool[] visited = new bool[words.Length];
+        Queue<(string, int)> queue = new Queue<(string, int)>();
+
+        queue.Enqueue((begin, 0));
+
+        while (queue.Count > 0)
+        {
+            var (currentWord, currentCount) = queue.Dequeue();
+
+            if (currentWord.Equals(target))
+            {
+                return currentCount;
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (WordUnmatch(currentWord, words[i]) > 1)
+                {
+                    continue;
+                }
+                
+                if (!visited[i])
+                {
+                    visited[i] = true;
+                    queue.Enqueue((words[i], currentCount + 1));
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public static int WordUnmatch(string target, string compare)
+    {
+        int wordUnmatch = 0;
+        
+        for (int j = 0; j < target.Length; j++)
+        {
+            if (target[j] != compare[j])
+            {
+                wordUnmatch++;
+            }
+        }
+
+        return wordUnmatch;
+    }
+}
