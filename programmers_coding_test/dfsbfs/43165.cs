eabd36d0e5d6f152c41516ea44a60867abc175b5
김현승 <corn120033@gmail.com>
2024-05-03 16:45:37 +0900

    깊이/너비 우선 탐색(DFS/BFS) > 타겟 넘버(DFS 사용, 속도 빠름, 평균 1.0ms)

---
+++ programmers_coding_test/dfsbfs/43165.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Solution {
+    static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        DFS(0, 0, numbers, target);
+        return answer;
+    }
+
+    public void DFS(int index, int data, int[] numbers, int target)
+    {
+        if (index == numbers.Length)
+        {
+            if (data == target)
+            {
+                answer++;
+            }
+            return;
+        }
+
+        DFS(index + 1, data + numbers[index], numbers, target);
+        DFS(index + 1, data - numbers[index], numbers, target);
+    }
+}
